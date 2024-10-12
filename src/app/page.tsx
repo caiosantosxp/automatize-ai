92336877e124117f0dd329000a9899fcae443ab2
unknown <caiosantosxp@gmail.com>
2024-10-11 22:05:03 -0300

    adicionando pdf exemples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-black">Ola</h1>
+      <ul>
+        <li>
+          <a href="">
+            https://5c71-2804-14c-f284-4388-b0a2-6da9-d3db-ae32.ngrok-free.app/extract_bl
+          </a>
+          <a href="">
+            https://5c71-2804-14c-f284-4388-b0a2-6da9-d3db-ae32.ngrok-free.app/extract_invoice
+          </a>
+          <a href="">
+            https://5c71-2804-14c-f284-4388-b0a2-6da9-d3db-ae32.ngrok-free.app/extract_packlist
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
